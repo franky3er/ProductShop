85f891d78a67501f0a413bee31cbcbfed3482137
franky3er <frankdreyer94@gmail.com>
2017-05-09 17:48:11 +0200

    master - add attribute db connection

--- src/vs/shopservice/ShopHandler.java
+++ src/vs/shopservice/ShopHandler.java
@@ -2,18 +2,26 @@ package vs.shopservice;
 
 import org.apache.thrift.TException;
 
+import java.sql.Connection;
+
 /**
  * Created by franky3er on 09.05.17.
  */
 public class ShopHandler implements ShopService.Iface {
 
+    private Connection connection;
+
+    public ShopHandler(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void ping() throws TException {
-
     }
 
     @Override
     public long fetchProductPrice(String productName, String productAmount) throws TException {
+        //TODO get product ammount of product..
 
         return 0;
     }
