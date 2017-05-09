bc801c416c9364abb6547e4bafe99677d93e5630
franky3er <frankdreyer94@gmail.com>
2017-05-09 16:12:48 +0200

    initial commit

---
+++ src/vs/productshop/main/MainApplication.java
@@ -0,0 +1,10 @@
+package vs.productshop.main;
+
+/**
+ * Created by franky3er on 09.05.17.
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        //TODO implement main
+    }
+}
