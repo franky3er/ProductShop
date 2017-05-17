99227f702dc5222735f8a57b8207320ee0b24916
franky3er <frankdreyer94@gmail.com>
2017-05-17 14:55:30 +0200

    feature/delivery - implement delivery

--- src/vs/products/shop/delivery/kalmund/KalmundDelivery.java
+++ src/vs/products/shop/delivery/kalmund/KalmundDelivery.java
@@ -2,13 +2,60 @@ package vs.products.shop.delivery.kalmund;
 
 import vs.products.shop.delivery.DeliveryOption;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 /**
  * KalmundDelivery is a simple testing delivery
  */
 public class KalmundDelivery implements DeliveryOption {
-
+    String deliveryDirectory;
+    
+    public KalmundDelivery(String deliveryDirectory) {
+        this.deliveryDirectory = deliveryDirectory;
+    }
+    
     @Override
     public void deliver(String productName, String productAmount, String deliveryAddress) {
+        String absolutDeliveryFilePath = createDeliveryAbsolutFilePath();
+        String deliveryFileContent = createDeliveryFileContent(productName, productAmount, deliveryAddress);
+        writeDeliveryFile(absolutDeliveryFilePath, deliveryFileContent);
+    }
 
+    private String createDeliveryFileContent(String productName, String productAmount, String deliveryAddress) {
+        String deliveryFileContentFormat = "DeliveryAddress: %s\n" +
+                "Product: %s\n" +
+                "Amount: %s";
+        String deliveryFileContent = String.format(
+                deliveryFileContentFormat,
+                deliveryAddress,
+                productName,
+                productAmount);
+        return deliveryAddress;
+    }
+
+    private String createDeliveryAbsolutFilePath() {
+        String fileName = Long.toString(System.currentTimeMillis()) + ".delivery";
+        return this.deliveryDirectory + File.separator + fileName;
+    }
+
+    private void writeDeliveryFile(String absolutDeliveryFilePath, String deliveryFileContent) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(absolutDeliveryFilePath, "UTF-8");
+            writer.println(deliveryFileContent);
+        } catch (FileNotFoundException e) {
+            System.err.println(String.format("ERROR : Can't write delivery: %s", absolutDeliveryFilePath));
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            System.err.println(String.format("ERROR : Can't write delivery: %s", absolutDeliveryFilePath));
+            e.printStackTrace();
+        } finally {
+            if(writer != null) {
+                writer.close();
+            }
+        }
     }
 }
